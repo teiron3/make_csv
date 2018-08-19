f26be41d124c8a3feb62b5f0d2a92782bc699a7d
teiron <teiron.teiron+git@gmail.com>
2018-08-20 07:22:22 +0900

    820

--- sub_form.cs
+++ sub_form.cs
@@ -25,3 +25,59 @@ partial class Form02 : Form{
         btn1.Click += (obj, e) => {p_args.Name = textbox1.Text;this.Close();};
     }
 }
+class Form03:Form{
+    PictureBox PictureBox1 = new PictureBox();
+    PictureBox PictureBox2 = new PictureBox();
+    int scrY = Screen.PrimaryScreen.Bounds.Height;
+    int scrX = Screen.PrimaryScreen.Bounds.Width;
+    int x  = Cursor.Position.X - 15;
+    int y  = Cursor.Position.Y - 15;
+    public form(){
+        this.Size = new Size(scrX, scrY);
+        this.Location = new Point(0, 0);
+        this.StartPosition = FormStartPosition.Manual;
+
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.TransparencyKey = this.BackColor;
+        
+        Console.WriteLine("x:{0} y:{1}", scrX, scrY);
+        PictureBox1.Parent = this;
+        PictureBox1.Size = new Size(31, 31);
+        PictureBox1.Location = new Point(x, y);
+        this.draw();
+
+        PictureBox2.Parent = this;
+        PictureBox2.Size = new Size(scrX, scrY);
+        PictureBox2.Location = new Point(0, 0);
+        this.draw2();
+        
+        this.KeyPreview = true;
+        KeyDown += new KeyEventHandler(keydown);
+    }
+    public void draw(){
+        Bitmap bmp = new Bitmap(31, 31);
+        PictureBox1.Image = bmp;
+        Graphics g = Graphics.FromImage(bmp);
+        g.DrawLine(Pens.Black, 15, 0, 15, 31);
+        g.DrawLine(Pens.Black, 0, 15, 31, 15);
+        g.Dispose();
+    }
+    public void draw2(){
+        Bitmap bmp = new Bitmap(scrX, scrY);
+        PictureBox2.Image = bmp;
+        Graphics g = Graphics.FromImage(bmp);
+        int x1 = 30, x2 = 60,y1 = 500, y2 = 400;
+        g.DrawRectangle(Pens.Black, (x1 < x2)?x1:x2,(y1 < y2)?y1:y2,(x1<x2)?x2-x1:x1-x2,(y1<y2)?y2-y1:y1-y2 );
+        g.Dispose();
+    }
+
+    void keydown(object sender, KeyEventArgs k){
+        if(k.KeyCode == Keys.J)y += 10;
+        if(k.KeyCode == Keys.K)y -= 10;
+        if(k.KeyCode == Keys.H)x -= 10;
+        if(k.KeyCode == Keys.L)x += 10;
+        Console.WriteLine("x:{0} y:{1}", x, y);
+
+        PictureBox1.Location = new Point(x, y);
+    }
+}
